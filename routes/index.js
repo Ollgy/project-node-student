bf30cd84bde5dd9eda34154ab5daa1c6ea55ebc6
Olga Baranova <bo_40388@mail.ru>
2019-07-11 15:17:16 +0300

    get

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,9 @@ const router = express.Router();
 
 const api = require('../api/index');
 
+router.get('/api/getUsers', api.getUsers);
+router.get('/api/getNews', api.getNews);
 router.get('/*', (req, res, next) => {
-    // if (err) {
-    //   console.log(err);
-    // }
-  
   res.sendFile(path.join(NODE_PATH + '/dist/index.html'));
 });
 
@@ -16,13 +14,13 @@ router.post('/api/saveNewUser', api.saveNewUser);
 router.post('/api/login', api.login);
 router.post('/api/authFromToken', api.authFromToken);
 router.post('/api/saveUserImage/:id', api.saveUserImage);
-router.put('/api/updateUser/:id', api.updateUser);
-router.delete('/api/deleteUser/:id', api.deleteUser);
-router.get('/api/getUsers', api.getUsers);
 router.post('/api/newNews', api.newNews);
-router.get('/api/getNews', api.getNews);
-router.delete('/api/deleteNews/:id', api.deleteNews);
+
+router.put('/api/updateUser/:id', api.updateUser);
 router.put('/api/updateNews/:id', api.updateNews);
 router.put('/api/updateUserPermission/:id', api.updateUserPermission);
+
+router.delete('/api/deleteUser/:id', api.deleteUser);
+router.delete('/api/deleteNews/:id', api.deleteNews);
 
 module.exports = router;
